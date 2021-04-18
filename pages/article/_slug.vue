d18f1ae8a8711182708e9ababf2465e9cce0d82a
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-04-18 09:34:30 +0700

    parameter optional

---
+++ pages/article/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span></span>
+    <main v-if="$route.params.slug">
+      <p>Article {{ $route.params.slug }}</p>
+    </main>
+    <main v-else>
+      <p>List Article</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
