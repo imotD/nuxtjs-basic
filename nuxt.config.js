1bb3d5b39f79f41cb807d2bdd96dc61c04eb21c1
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-08 11:08:10 +0700

    Populate Home Page With Data

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,10 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/axios'],
+  axios: {
+    // proxy: true
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
