d1042d5bb76090157157ce931617770867779327
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-10 15:03:22 +0700

    serach fetaure

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,51 @@
 <template>
   <div class="home">
     <Hero />
+
+    <div class="container search">
+      <input @keyup.enter="$fetch" v-model.lazy="searchInput" type="text" placeholder="Search">
+      <button @click="clearSearch" v-show="searchInput !== ''" class="button">
+        Clear Search
+      </button>
+    </div>
+
     <div class="container movies">
-      <div id="movie-grid" class="movies-grid">
+      <div v-if="searchInput !== ''" id="movie-grid" class="movies-grid">
+        <div v-for="(movie,index) in searchedMovies" :key="index" class="movie">
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"/>
+            <p class="review">
+              {{ movie.vote_average }}
+            </p>
+            <p class="overview">
+              {{ movie.overview }}
+            </p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0,25) }}
+              <span v-if="movie.title.length > 25">...</span>
+            </p>
+            <p class="release">
+              Released :
+             {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <NuxtLink
+              class="button button-light"
+              :to="{ name: 'movies-id', params: { id: movie.id } }"
+            >
+              Get More Info
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div v-else id="movie-grid" class="movies-grid">
         <div v-for="(movie,index) in movies" :key="index" class="movie">
           <div class="movie-img">
             <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"/>
@@ -47,11 +90,17 @@ export default {
   name: 'IndexPage',
   data () {
     return {
-      movies: []
+      movies: [],
+      searchInput: '',
+      searchedMovies: []
     }
   },
   async fetch () {
-    await this.getMovies()
+    if (this.searchInput === '') {
+      await this.getMovies()
+      return
+    }
+    await this.searchMovies()
   },
   methods: {
     async getMovies () {
@@ -60,7 +109,20 @@ export default {
       result.data.results.forEach((movie) => {
         this.movies.push(movie)
       })
-      console.log(result.data)
+      // console.log(result.data)
+    },
+    async searchMovies () {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1&query=${this.searchInput}`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.searchedMovies.push(movie)
+      })
+    },
+    clearSearch () {
+      this.searchInput = ''
+      this.searchedMovies = ''
     }
   }
 }
