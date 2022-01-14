f84d69dac93f79adfd75db44da4b8633684615f0
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-14 10:53:17 +0700

    init project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,18 @@
 export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxtBasic',
+    title: 'spaceapp',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
